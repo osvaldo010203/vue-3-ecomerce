d2f6f67ddba327985bb2cd8ecd0b60d1df397ca7
locop <locoporque@hotmail.com>
2024-03-05 13:21:08 -0600

    Add basic examples with Options API and Compoistion API

---
+++ src/examples/ContadorComposition.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+    import { onMounted, ref } from 'vue'
+    const count = ref(0);
+    function aumentar(){
+        count.value+=1;
+    }
+    onMounted(() =>{
+        console.log('El valor del contador es '+ count.value)
+    });
+</script>
+<template>
+    <p>Hola soy una composision de Contador API</p>
+    <h1>Contador:  {{ count }}</h1>
+    <button @click="aumentar">
+        no hace nada
+    </button>
+</template>
